ab0d2a6f437c82a05e7848432363e5fe6c4f407a
LuciOus-G <void341@gmail.com>
2021-05-15 01:32:33 +0700

    add CI on heroku

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -15,13 +15,13 @@ class Landing_page_modification(admin.ModelAdmin):
     #make field disable cannot be edited
     readonly_fields = ['name']
 
-    # This will help you to disbale add functionality
-    def has_add_permission(self, request):
-        return False
-
-    # This will help you to disable delete functionality
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # # This will help you to disbale add functionality
+    # def has_add_permission(self, request):
+    #     return False
+    #
+    # # This will help you to disable delete functionality
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
     class Meta:
         model = models.Landing_page
