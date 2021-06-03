91b82823433c108879e72f3fa9f21b939436ae06
mochamad iqbal <iqbal.mochamad@gramedia.id>
2021-06-03 20:16:05 +0700

    dashboard

--- sponsor_dashboard/urls.py
+++ sponsor_dashboard/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 #this url patterns is looking to views.py in blog app directory
 urlpatterns = [
-    path('sponsor/dahsboard', views.dashboard, name='dashboard')
+    path('sponsor/dahsboard/maps', views.dashboard_maps, name='dashboard_maps'),
+    path('sponsor/dahsboard/profile', views.dashboard_profile, name='dashboard_profile'),
+    path('sponsor/dahsboard/gallery', views.dashboard_galeery, name='dashboard_gallery')
 ]
