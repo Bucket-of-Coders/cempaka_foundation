cc8ae53f0a37a8d19805f1e0f9f5a2a01298f011
Mochamad Iqbal Al Jihad <54808710+LuciOus-G@users.noreply.github.com>
2021-09-04 14:29:03 +0700

    css
    load css

--- backend_yayasan_cempaka/settings.py
+++ backend_yayasan_cempaka/settings.py
@@ -151,9 +151,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 ## uncomment if want to run on local
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "staticfiles")
+# ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
